46aeb9e47fb89d64b8e874fdefe95ec7d59a84aa
yangpengbo11 <470767987@qq.com>
2019-06-05 09:26:25 +0800

    删除无限递归

--- application/admin/controller/Menu.php
+++ application/admin/controller/Menu.php
@@ -35,19 +35,8 @@ class Menu extends Base
         return $this->fetch('add_menu');
     }
 
-    //无限极递归
-    public function getTree($data,$pid=0,$level=0){
-        static $arr=array();
-        foreach($data as $key=>$value){
-            if($value['pid'] == $pid){
-                $value['level']=$level;     //用来作为在模版进行层级的区分
-                $arr[] = $value;            //把内容存进去
-                $this->getTree($data,$value['id'],$level+1);    //回调进行无线递归
-            }
-        }
-        return $arr;
 
-    }
+
 
     /**
      * post增加修改菜单
