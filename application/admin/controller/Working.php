e5a5883d3878efc19faa463dca4d9d0698dbc180
liuyulong <wo_635798673@163.com>
2019-06-13 15:57:55 +0800

    增加工序流程配置添加修改功能

--- application/admin/controller/Working.php
+++ application/admin/controller/Working.php
@@ -51,8 +51,78 @@ class Working extends Base
          }
      }
 
+    /**
+     * 工序流程配置列表
+     */
+     public function distribute_list(){
+         $list = db('process_flow')->select();
+         $this->assign('list',$list);
+        return $this->fetch('distribute_list');
+     }
 
+    /**
+     * 工序流程配置添加
+     */
+    public function distribute_add(){
+        $list = db('process')->select();
+        $this->assign('data','');
+        $this->assign('process',$list);
+        return $this->fetch('distribute_post');
+    }
+    /**
+     * 工序流程配置编辑
+     */
+    public function distribute_edit(){
+        $id = input('id');
+        $data = db('process_flow')->where('id',$id)->find();
+        $list = db('process')->select();
+        $this->assign('data',$data);
+        $this->assign('process',$list);
+        return $this->fetch('distribute_post');
+    }
 
+    /**
+     * 工序流程配置添加/编辑post提交
+     */
+    public function distribute_post(){
+        if(empty($_POST['id'])){
+            $data = array(
+                'process_id' => $_POST['process_id'],
+                'inventory_class_id' =>$_POST['inventory_class_id'],
+                'orders' => $_POST['orders'],
+                'create_time'=>date('Y-d-m H:i:s',time())
+            );
+            $res = db('process_flow')->insert($data);
+            if($res){
+                $this->success('添加成功.', 'Working/distribute_list');
+            }else{
+                $this->error('添加失败！');
+            }
+        }else{
+            $data = array(
+                'process_id' => $_POST['process_id'],
+                'inventory_class_id' =>$_POST['inventory_class_id'],
+                'orders' => $_POST['orders']
+            );
+            $res = db('process_flow')->where('id',$_POST['id'])->update($data);
+            if($res){
+                $this->success('编辑成功.', 'Working/distribute_list');
+            }else{
+                $this->error('编辑失败！');
+            }
+        }
+    }
+
+    /**
+     * 搜索显示数据
+     * @return
+     */
+    public function vague(){
+
+        $vague = input('vague');
+        $list = $this->getVague('inventory_class','inventory_class_name',$vague);
+        return json($list);
+    }
 
 
 }
