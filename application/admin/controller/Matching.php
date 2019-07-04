1e76ba25fb67bcf81a7847777d145ed6aeab0785
liuyulong <wo_635798673@163.com>
2019-07-04 15:21:29 +0800

    完成配套检查分级显示部件位置

--- application/admin/controller/Matching.php
+++ application/admin/controller/Matching.php
@@ -48,23 +48,26 @@ class Matching extends Base
         return json_encode($arrs,JSON_UNESCAPED_UNICODE);
     }
 
+    public $arr_s=array();
     public function arrs($data){
         foreach ($data as $k=>$v){
-            $arr['id'] = $v['id'];
-            $arr['pId'] = $v['pid'];
-            $ss = db('inventory')->where('inventory_code',$v['base_code'])->find();
-            if(!empty($ss)){
-                $arr['name']= $ss['inventory_name'].'('.$v['specification_type'].')('.$v['counts'].')';
-            }else{
-                $arr['name']= $v['inventory_class_name'].'('.$v['specification_type'].')('.$v['counts'].')';
+            if(!empty($data[$k])) {
+                $arr['id'] = $v['id'];
+                $arr['pId'] = $v['pid'];
+                $ss = db('inventory')->where('inventory_code', $v['base_code'])->find();
+                if (!empty($ss)) {
+                    $arr['name'] = $ss['inventory_name'] . '(' . $v['specification_type'] . ')(' . $v['counts'] . ')';
+                } else {
+                    $arr['name'] = $v['inventory_class_name'] . '(' . $v['specification_type'] . ')(' . $v['counts'] . ')';
+                }
+                $arr['open'] = false;
+                if (!empty($v['children'])) {
+                    $arr['children'] = $this->arrs($v['children']);
+                }
+                $this->arr_s[] = $arr;
             }
-            $arr['open']=false;
-            if(!empty($v['children'])){
-                $arr['children'] = $this->arrs($v['children']);
-            }
-            $arrs[] = $arr;
         }
-        return $arrs;
+        return $this->arr_s;
     }
 
     /**
@@ -76,33 +79,37 @@ class Matching extends Base
         $product = db('qrcode_record')->where('id',$id)->find();
         $product_list = db('qrcode_record')
             ->alias('a')
-            ->join('tf_process b','b.id = a.process_flow_id')
+            ->join('tf_process b', 'a.process_flow_id = b.id')
             ->where('a.base_code',$product['base_code'])
             ->where('a.pid',0)
             ->where('a.specification_type',$product['specification_type'])
             ->select();
         return json_encode($product_list);
     }
-
-    public function isQrcodeRecord($id,$number,$process_flow_id=''){
+    public $data1=array();
+    public function isQrcodeRecord($id,$number,$process_flow_id=0){
         $product_list = db('products')->where('pid',$id)->select();
+        $data = array();
         foreach ($product_list as $k=>$v){
             $where=array();
             if(!empty($process_flow_id)){
                 $where['process_flow_id'] = $process_flow_id;
             }
-            $data1[]= db('qrcode_record')
+            $find = db('qrcode_record')
                 ->distinct('base_code')
                 ->field('*,count(base_code) as counts')
                 ->where('base_code',$v['inventory_code'])
                 ->where('pid','0')
                 ->where($where)
                 ->group('base_code')->find();
-            $product_lists = db('products')->where('pid',$v['id'])->find();
-            if(!empty($product_lists)&&$data1[$k]['counts']<$number){
-                $data1[]['children'] = $this->isQrcodeRecord($v['id'],$number);
+            if(!empty($find)){
+                $product_lists = db('products')->where('pid',$v['id'])->find();
+                if(!empty($product_lists)|| $find['counts']<$number){
+                    $find['children'] = $this->isQrcodeRecord($v['id'],$number);
+                }
+                $data[$k] = $find;
             }
         }
-        return $data1;
+        return  $data;
     }
 }
